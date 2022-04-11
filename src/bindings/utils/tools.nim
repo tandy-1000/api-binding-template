4393bbea45acadd9ad076f15a911f49572b08044
tandy <tandy1000@cock.li>
2022-04-11 17:44:06 +0100

    add optional types dump hooks

--- src/bindings/utils/tools.nim
+++ src/bindings/utils/tools.nim
@@ -19,3 +19,42 @@ template dumpKey*(s: var string, v: string) =
   # jsony hook to convert from camelCase to snake_case
   const v2 = v.camel2snake().toJson() & ":"
   s.add v2
+
+## Don't serialise optional types which are empty
+
+proc dumpHook*[T](s: var string, v: Option[T]) =
+  # jsony dump hook for option types
+  if v.isSome:
+    s.dumpHook(v.get())
+
+proc dumpHook*(s: var string, v: object) =
+  # jsony dump hook to drop option types
+  s.add '{'
+  var i = 0
+  when compiles(for k, e in v.pairs: discard):
+    # Tables and table like objects.
+    for k, e in v.pairs:
+      if i > 0:
+        s.add ','
+      s.dumpHook(k)
+      s.add ':'
+      s.dumpHook(e)
+      inc i
+  else:
+    # Normal objects.
+    for k, e in v.fieldPairs:
+      when compiles(e.isSome):
+        if e.isSome:
+          if i > 0:
+            s.add ','
+          s.dumpKey(k)
+          s.dumpHook(e)
+          inc i
+      else:
+        if i > 0:
+          s.add ','
+        s.dumpKey(k)
+        s.dumpHook(e)
+        inc i
+  s.add '}'
+
